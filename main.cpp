44c77c10192d76dc4ec43f0924f3e5851a6bae16
Daniel Mendoza <danny105405@gmail.com>
2024-04-20 15:33:54 -0500

    Outlines of functions

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Prime number validation function
+bool isPrime(int x, int y){
+}
+
+// Totient function --> O
+
+// Modulo function --> n
+
+// Generate keys function
+
+// Verify keys function
+
+int main(){
+
+    // User input for two prime numbers: p & q
+    int p;
+    int q;
+    cout << "Enter a prime number in the range of 5,000 to 10,000: " << endl;
+    cin >> p;
+    cout << "Enter another prime number in the range of 5,000 to 10,000: " << endl;
+    cin >> q;
+
+    // Validate range of 5000 - 10000
+
+    // Compute modulo n = p * q
+    // Totient function value O = (p-1)(q-1)
+
+    // Generate a private 'd' and public key 'e' based on n and O
+    // Verify keys are multiplicative inverses of each other based on O
+
+    return 0;
+}
