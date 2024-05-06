27223da4172c776540ba514f377e8c4c358df3c7
Daniel Mendoza <danny105405@gmail.com>
2024-05-05 21:59:50 -0500

    Finished project

--- main.cpp
+++ main.cpp
@@ -2,52 +2,71 @@
 #include <algorithm>
 using namespace std;
 
-// This function checks to see if a value is prime 
-bool Prime(int x){
-    if (x < 2)
+// This function checks to see if a value is prime number and returns status
+bool Prime(int val)
+{
+    if (val < 2)
         return false;
-    for (int i = 2; i <= x/2; i++){
-        if (x % i == 0){
+    for (int i = 2; i <= val/2; i++){
+        if (val % i == 0){
             return false;
         }
     }
     return true;
 }
 
-// This function calculates the modulus value --> n
-int Mod(int x, int y){
-    int n = x * y;
-    return n;
+// This function calculates the modulus value --> n and returns 'n'
+int Mod(int val1, int val2)
+{
+    // n = p * q
+    int modulus = val1 * val2;
+    return modulus;
 }
 
-// This function calculates the totient value --> O
-int Totient(int x, int y){
-    int phi = ((x-1)*(y-1));
+// This function calculates the totient value --> O and returns 'O'
+int Totient(int val1, int val2)
+{
+    // O = (p-1)*(q-1)
+    int phi = ((val1-1)*(val2-1));
     return phi;
 }
 
-// This function generates the public key --> e
-int PublicKey(int phi){
-    int e;
-    for (e = 2; e < phi; e++){
-        if (__algo_gcd(e, phi) == 1){
+// This function calculates the greatest common divisor between two values and returns GCD
+int GCD(int num1, int num2)
+{
+    if (num1 == 0){
+        return num2;
+    }
+    return GCD(num2 % num1, num1);
+}
+
+// This function generates the public key --> e and returns 'e' or an error if there is not a valid e
+int PublicKey(int phi)
+{
+    // Loop to check valid public keys
+    for (int e = 2; e < phi; e++){
+        // Condition to check if the public key is a relative prime of totient value
+        if (GCD(e, phi) == 1){
             return e;
         }
     }
+    // If no valid public key, return an error
     return -1;
 }
 
-// This function generates the private key --> d
+// This function generates the private key --> d and returns 'd'
 int PrivateKey(int e, int phi){
     int d = 2;
+    // Loop to generate a private key that is valid
     while ((d * e) % phi != 1) {
         d++;
     }
     return d;
 }
 
-// This function verifies that the keys are multiplicative inverse of totient value
+// This function verifies that the keys are multiplicative inverse of totient value or if they are not
 void verifyKeys(int e, int d, int phi){
+    // Condition with multiplicative inverse algorithm
     if ((e * d) % phi == 1){
         cout << "The keys are mulitplicative inverse of " << phi << endl;
     }
@@ -57,7 +76,7 @@ void verifyKeys(int e, int d, int phi){
 }
 
 int main(){
-    // User input for two prime numbers: p & q
+    // User input for two prime numbers --> p & q
     int p;
     int q;
     cout << "Enter a prime number in the range of 5,000 to 10,000: " << endl;
@@ -65,34 +84,37 @@ int main(){
     cout << "Enter another prime number in the range of 5,000 to 10,000: " << endl;
     cin >> q;
 
-    // Validate user input range between 5000 - 10000
+    // Validate user input range between 5000 - 10000. 
     if ((p < 5000 || p > 10000) || (q < 5000 || q > 10000)){
-        cout << "Error: Both values are not inside the specified range" << endl;
+        cout << "Error: Both values must be inside the specified range " << endl;
         return -1;
     }
     // Validate user input values are prime 
-    else if (!Prime(p) || !Prime(q)){
-        cout << "Error: Both values must be prime. " << endl;
+    else if ((Prime(p) == false) || (Prime(q) == false)){
+        cout << "Error: Both values must be prime " << endl;
         return -1;
     }
+    else{
+        cout << "Both values are inside the specified range and are prime " << endl;
+    }
 
-    // Display modulus value --> n
+    // Call Mod function and display modulus value --> n
     int n = Mod(p,q);
-    cout << "n: " << n << endl;
+    cout << "The modulus is : " << n << endl;
 
-    // Display totient function value --> O
+    // Call Totient function and display totient function value --> O
     int O = Totient(p,q);
-    cout << "O: " << O << endl;
+    cout << "The totient value is : " << O << endl;
 
-    // Generate and display the public key --> e
+    // Call PublicKey function and display the public key --> e
     int e = PublicKey(O);
     cout << "The public key is: <" << e << ", " << n << ">" << endl;
 
-    // Generate and display the private key --> d
+    // Call PrivateKey function and display the private key --> d
     int d = PrivateKey(e, O);
     cout << "The private key is: <"  << d << ", " << n << ">" << endl;
 
-    // Verify keys e and d are multiplicative inverses for O
+    // Call verifyKeys function to verify keys 'e' and 'd' are multiplicative inverses for O
     verifyKeys(e, d, O);
 
     return 0;
